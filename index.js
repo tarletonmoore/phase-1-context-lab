509675a18eb8d81c93b2682aa6b23bd5738dd3bf
John Tarleton Moore <tarletonmoore@gmail.com>
2022-04-19 14:52:50 -0400

    commit tree 6ff82ca6d9478fdde3c857c3c3cb32f139abdb44

--- index.js
+++ index.js
@@ -1,4 +1,84 @@
 /* Your Code Here */
+function createEmployeeRecord(array) {
+
+    let object = {
+        firstName: array[0],
+        familyName: array[1],
+        title: array[2],
+        payPerHour: array[3],
+        timeInEvents: [],
+        timeOutEvents: [],
+
+    }
+    return object
+}
+
+function createEmployeeRecords(array) {
+
+    return array.map(element => createEmployeeRecord(element))
+}
+
+function createTimeInEvent(date) {
+
+    let time = date.split(` `)[1]
+    let dates = date.split(` `)[0]
+    let timeInObject = {
+        type: "TimeIn",
+        hour: parseInt(time),
+        date: dates,
+    }
+    this.timeInEvents.push(timeInObject)
+    return this
+}
+
+function createTimeOutEvent(date) {
+    let timeOutObject = {
+        type: "TimeOut",
+        hour: parseInt(date.split(` `)[1]),
+        date: date.split(` `)[0],
+    }
+    this.timeOutEvents.push(timeOutObject)
+    return this
+}
+
+function hoursWorkedOnDate(date) {
+    const timeIn = this.timeInEvents.find(element => element.date === date)
+
+    const timeOut = this.timeOutEvents.find(element => element.date === date)
+    return (timeOut.hour - timeIn.hour) / 100
+}
+
+function wagesEarnedOnDate(date) {
+    // const timeIn = this.timeInEvents.find(element => element.date === date)
+    // const timeOut = this.timeOutEvents.find(element => element.date === date)
+    const wages = this.payPerHour
+    console.log("wages", wages)
+    // return (timeOut.hour - timeIn.hour) / 100 
+    return hoursWorkedOnDate.call(this, date) * wages
+
+}
+
+// function allWagesFor() {
+//     let allWages = this.timeInEvents.map(element => wagesEarnedOnDate(this, element.date))
+//     console.log("wages", allWages)
+//     let total = 0
+//     allWages.forEach(element => total = element + total)
+//     return total
+// }
+
+function findEmployeeByFirstName(srcArray, firstName) {
+    console.log("new", srcArray, firstName)
+    return srcArray.find(element => element.firstName === firstName)
+}
+
+function calculatePayroll(array) {
+    let sum = array.reduce((preValue, curValue) => {
+
+        return preValue + allWagesFor.call(curValue)
+    }, 0)
+    return parseInt(sum)
+    // return array.forEach(element => element.firstName + allWagesFor.call(this))
+}
 
 /*
  We're giving you this function. Take a look at it, you might see some usage
